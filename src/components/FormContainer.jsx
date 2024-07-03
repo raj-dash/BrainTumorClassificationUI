f5df068a33a99f537a30e28a17040a6e66c69e1f
Raj Aryan Dash <rajaryanrad@gmail.com>
2024-07-04 01:58:46 +0530

    Added label component

--- src/components/FormContainer.jsx
+++ src/components/FormContainer.jsx
@@ -1,9 +1,13 @@
+import Label from "./Label";
+
 function FormContainer() {
   return (
     <>
       <div className="container">
         <div className="form-container">
-          <form action=""></form>
+          <form action="">
+            <Label />
+          </form>
         </div>
       </div>
     </>
