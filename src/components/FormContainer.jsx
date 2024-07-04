46fc2c1b04086063d06c82b993ea647c9b1245ee
Raj Aryan Dash <rajaryanrad@gmail.com>
2024-07-04 20:37:56 +0530

    Added image input component

--- src/components/FormContainer.jsx
+++ src/components/FormContainer.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
+import ImageUpload from "./ImageUpload";
 import Label from "./Label";
 
 function FormContainer() {
+  const [selectedFile, setSelectedFile] = useState();
   return (
     <>
       <div className="container">
         <div className="form-container">
           <form action="">
             <Label />
+            <ImageUpload
+              selectedFile={selectedFile}
+              setSelectedFile={setSelectedFile}
+            />
           </form>
         </div>
       </div>
