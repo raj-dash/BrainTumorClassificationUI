b359a88797069b2c0e0004d724db929ffdf5f659
Raj Aryan Dash <rajaryanrad@gmail.com>
2024-07-05 19:34:36 +0530

    Fixed issue with api and it is now working

--- src/components/FormContainer.jsx
+++ src/components/FormContainer.jsx
@@ -1,21 +1,50 @@
 import { useState } from "react";
-import ImageUpload from "./ImageUpload";
-import Label from "./Label";
-import Submit from "./Submit";
 
 function FormContainer() {
-  const [selectedFile, setSelectedFile] = useState();
+  const [text, setText] = useState("Please enter your Image");
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  function handleFileSelect(event) {
+    setSelectedFile(event.target.files[0]);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData();
+    fd.append("image", selectedFile);
+
+    try {
+      const endpoint = "http://127.0.0.1:8000/uploadfile/";
+      const response = await fetch(endpoint, {
+        method: "POST",
+        body: fd,
+      });
+
+      if (response.ok) {
+        console.log("file uploaded successfully");
+      } else {
+        console.error("failed to upload file");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <>
       <div className="container">
         <div className="form-container">
-          <form action="">
-            <Label />
-            <ImageUpload
-              selectedFile={selectedFile}
-              setSelectedFile={setSelectedFile}
-            />
-            <Submit selectedFile={selectedFile} />
+          <form action="POST" onSubmit={handleSubmit}>
+            <div>
+              <label className="label">{text}</label>
+            </div>
+            <div>
+              <input type="file" name="image" onChange={handleFileSelect} />
+            </div>
+            <div>
+              <button type="submit">Submit</button>
+            </div>
           </form>
         </div>
       </div>
