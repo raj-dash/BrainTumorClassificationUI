d54cbec872ee4b9201c0cb16db232f321ca0b125
Raj Aryan Dash <rajaryanrad@gmail.com>
2024-07-05 23:03:18 +0530

    The api and the website connection is not working correctly

--- src/components/FormContainer.jsx
+++ src/components/FormContainer.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 function FormContainer() {
-  const [text, setText] = useState("Please enter your Image");
+  const [text, setText] = useState("");
   const [selectedFile, setSelectedFile] = useState(null);
 
   function handleFileSelect(event) {
@@ -19,16 +19,35 @@ function FormContainer() {
       const response = await fetch(endpoint, {
         method: "POST",
         body: fd,
-      });
+      })
+        .then((response) => response.json())
+        .then((data) => getTumor(data["prediction"]));
 
-      if (response.ok) {
-        console.log("file uploaded successfully");
-      } else {
-        console.error("failed to upload file");
-      }
+      // if (response.ok) {
+      //   console.log("file uploaded successfully");
+      //   console.log(response);
+      // } else {
+      //   console.error("failed to upload file");
+      // }
     } catch (error) {
       console.error(error);
     }
+  }
+
+  function getTumor(prediction) {
+    let tumor = "";
+    switch (prediction) {
+      case "glioma_tumor":
+        tumor = "Glioma Tumor";
+        break;
+      case "meningioma_tumor":
+        tumor = "Meningioma Tumor";
+      case "no_tumor":
+        tumor = "No Tumor";
+      case "pituitary_tumor":
+        tumor = "Pituitary Tumor";
+    }
+    setText(tumor);
   }
 
   return (
@@ -37,13 +56,16 @@ function FormContainer() {
         <div className="form-container">
           <form action="POST" onSubmit={handleSubmit}>
             <div>
-              <label className="label">{text}</label>
+              <label className="label">Please enter your image</label>
             </div>
             <div>
               <input type="file" name="image" onChange={handleFileSelect} />
             </div>
             <div>
               <button type="submit">Submit</button>
+            </div>
+            <div>
+              <h2>{text}</h2>
             </div>
           </form>
         </div>
