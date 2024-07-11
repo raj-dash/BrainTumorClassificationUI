89c1decd21b7a664a47d9568acada236d54ee4c8
Raj Aryan Dash <rajaryanrad@gmail.com>
2024-07-11 11:39:58 +0530

    squares

--- src/components/BackgroundAnimation.jsx
+++ src/components/BackgroundAnimation.jsx
@@ -1,12 +1,19 @@
 function BackgroundAnimation() {
   return (
     <>
-      <div class="ripple-background">
-        <div class="circle xxlarge shade1"></div>
-        <div class="circle xlarge shade2"></div>
-        <div class="circle large shade3"></div>
-        <div class="circle medium shade4"></div>
-        <div class="circle small shade5"></div>
+      <div class="area">
+        <ul class="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
       </div>
     </>
   );
