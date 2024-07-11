091c3c4cbd9beb2e23d882a164baa416b4774381
Raj Aryan Dash <rajaryanrad@gmail.com>
2024-07-11 11:10:51 +0530

    Added background component

---
+++ src/components/BackgroundAnimation.jsx
@@ -0,0 +1,15 @@
+function BackgroundAnimation() {
+  return (
+    <>
+      <div class="ripple-background">
+        <div class="circle xxlarge shade1"></div>
+        <div class="circle xlarge shade2"></div>
+        <div class="circle large shade3"></div>
+        <div class="circle medium shade4"></div>
+        <div class="circle small shade5"></div>
+      </div>
+    </>
+  );
+}
+
+export default BackgroundAnimation;
